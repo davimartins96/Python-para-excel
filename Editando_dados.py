2239a3cc9b388b6255d7f6caa917d820b3c8df4d
Thallysson-br <thallysson.maldonado@gmail.com>
2022-06-24 10:13:29 -0300

    atualizando tabela de vendas

---
+++ Editando_dados.py
@@ -0,0 +1,12 @@
+################################### Importando bibliotecas ########################################
+
+import pandas as pd
+from IPython.display import display
+
+###################################################################################################
+
+
+#metodo .head(número de linhas iniciais que você pretende ver no terminal)
+#metodo .shape mostra quantas linhas e colunas possui o seu banco de dados
+#metodo .describe() mostra vários parametros da tabela como contar os valores numericos
+#mostrar os máximos e minimos e a média dos valores na tabela e descio padrão std
